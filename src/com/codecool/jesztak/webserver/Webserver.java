90aa5647c204dae195e4d3582813699c41508ace
martaJesztak <jesztakm@gmail.com>
2018-06-18 10:45:05 +0200

    Refactoring

--- src/com/codecool/jesztak/webserver/Webserver.java
+++ src/com/codecool/jesztak/webserver/Webserver.java
@@ -2,6 +2,7 @@ package com.codecool.jesztak.webserver;
 
 import com.codecool.jesztak.routes.Routes;
 import com.codecool.jesztak.routes.WebRoute;
+import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
@@ -27,12 +28,12 @@ public class Webserver {
 
     public void initialize() throws IOException {
         server = HttpServer.create(new InetSocketAddress(8000), 0);
-        setupContext();
+        findRoute();
         server.setExecutor(null); // creates a default executor
         server.start();
     }
 
-    public void setupContext() {
+    public void findRoute() {
         Method[] routes = routeClass.getDeclaredMethods();
         for (Method method : routes) {
             if (method.isAnnotationPresent(WebRoute.class)) {
@@ -40,10 +41,7 @@ public class Webserver {
                 server.createContext(path, httpExchange -> {
                     try {
                         String response = (String)method.invoke(new Routes());
-                        httpExchange.sendResponseHeaders(200, response.length());
-                        OutputStream os = httpExchange.getResponseBody();
-                        os.write(response.getBytes());
-                        os.close();
+                        setupContext(httpExchange, response);
                     } catch (IllegalAccessException | InvocationTargetException e) {
                         System.out.println("BAD");
                     }
@@ -51,6 +49,13 @@ public class Webserver {
             }
 
         }
+    }
+
+    private void setupContext(HttpExchange httpExchange, String response) throws IOException {
+        httpExchange.sendResponseHeaders(200, response.length());
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
     }
 
     public static void main(String[] args) throws Exception {
